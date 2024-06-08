d27611bd27d52330a0f02872fc84aa27f273bb68
gabrielhipolitoo <c.gabrielhipolitoh@gmail.com>
2024-06-08 16:18:01 -0300

    change link

--- src/Componentes/Nav.js
+++ src/Componentes/Nav.js
@@ -1,146 +1,129 @@
 import React, { useEffect, useState } from "react";
-import  "../assets/css/nav.css";
+import "../assets/css/nav.css";
 import logo from "../assets/imgs/logo-aceff.png";
 import { useLocation } from "react-router-dom";
 
-
-
-
 //fontawasome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
-
-  
   // Hooks
   const location = useLocation();
-  const [path,setPath] = useState()
-  const [background,setbackground] = useState()
-  const [menuActive,setMenuActive] = useState(false)
-  const [color,setColor] =  useState()
-  const [colorIcon,setColorIcon] = useState({color:"#fff"})
-
+  const [path, setPath] = useState();
+  const [background, setbackground] = useState();
+  const [menuActive, setMenuActive] = useState(false);
+  const [color, setColor] = useState();
+  const [colorIcon, setColorIcon] = useState({ color: "#fff" });
 
   //array links
   const arrayLinks = [
     {
-      href:"https://aceffong.com.br/",
-      texto:"Início"
+      href: "https://aceffong.com.br/",
+      texto: "Início",
     },
     {
-      href:"https://aceffong.com.br/sobre/",
-      texto:"Sobre"
+      href: "https://aceffong.com.br/sobre/",
+      texto: "Sobre",
     },
     {
-      href:"https://aceffong.com.br/galeria/",
-      texto:"Galeria"
+      href: "https://aceffong.com.br/galeria/",
+      texto: "Galeria",
     },
 
     {
-      href:"https://aceffong.com.br/projetos/",
-      texto:"Projetos"
+      href: "https://aceffong.com.br/projetos/",
+      texto: "Projetos",
     },
     {
-      href:"https://aceffong.com.br/empresas-parceiras/",
-      texto:"Empresas Parceiras"
+      href: "https://aceffong.com.br/empresas-parceiras/",
+      texto: "Empresas Parceiras",
     },
     {
-      href:"https://aceffong.com.br/certificados/",
-      texto:"Certficados"
+      href: "https://aceffong.com.br/certificados/",
+      texto: "Certficados",
     },
     {
-      href:"https://aceffong.com.br/doe/",
-      texto:"Doe"
+      href: "https://aceffong.com.br/doe/",
+      texto: "Faça sua doaçao",
     },
     {
-      href:"https://aceffong.com.br/contato/",
-      texto:"Contato"
-    }
-  ]
+      href: "https://aceffong.com.br/contato/",
+      texto: "Contato",
+    },
+  ];
 
   // functions
   const menuMobile = () => {
-    setMenuActive(!menuActive)
-  }
-
+    setMenuActive(!menuActive);
+  };
 
   const handleScroll = () => {
-
-    if(window.scrollY >=40){
-
-        setbackground({
-        backgroundColor:"#fff",
-        boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)"
-      })
+    if (window.scrollY >= 40) {
+      setbackground({
+        backgroundColor: "#fff",
+        boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
+      });
       setColor({
-        color:"#364753", 
-      }) 
-
-
-      
-    }
-    else{
+        color: "#364753",
+      });
+    } else {
       setColor({
-        color:"#fff", 
-      }) 
-      setbackground({})
+        color: "#fff",
+      });
+      setbackground({});
     }
-  }
-
-  
+  };
 
   //useffects
 
   useEffect(() => {
     const baseStyle = () => {
-      if(path === "/"){
-        setbackground({})
-       window.addEventListener(("scroll"),handleScroll)
-       setColor({color:"#fff"})
-      }
-  
-      else{
+      if (path === "/") {
+        setbackground({});
+        window.addEventListener("scroll", handleScroll);
+        setColor({ color: "#fff" });
+      } else {
         setbackground({
-          backgroundColor:"#fff",
-          boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)"
-        })
+          backgroundColor: "#fff",
+          boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
+        });
         setColor({
-          color:"#364753", 
-        }) 
-  
+          color: "#364753",
+        });
       }
-    }
-    baseStyle()
+    };
+    baseStyle();
 
     return () => {
-      window.removeEventListener(("scroll"),handleScroll)
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [path]);
 
-    }
-  },[path])
-
-  useEffect(()=> {
-    setPath(location.pathname)
-  },[location])
+  useEffect(() => {
+    setPath(location.pathname);
+  }, [location]);
 
   return (
     <nav
-    style={background}
+      style={background}
       id="nav_"
-      className={`nav_links ${menuActive? "active":""}`}>
-
+      className={`nav_links ${menuActive ? "active" : ""}`}
+    >
       <img src={logo} alt="logo da pagina" />
-      <button onClick={menuMobile}  id="btnMobile" >
-      <FontAwesomeIcon fontSize="3rem" style={color}  icon={faBars} />
+      <button onClick={menuMobile} id="btnMobile">
+        <FontAwesomeIcon fontSize="3rem" style={color} icon={faBars} />
       </button>
       <div className="links">
         <button onClick={menuMobile} id="fecharMobile">
-        <FontAwesomeIcon fontSize="3rem" color="#364753" icon={faXmark} />
+          <FontAwesomeIcon fontSize="3rem" color="#364753" icon={faXmark} />
         </button>
         <ul>
-          {arrayLinks.map(({texto,href}) =>(
+          {arrayLinks.map(({ texto, href }) => (
             <li key={texto}>
-              <a style={menuActive? {color:"#364753"}:color} href={href}>{texto}</a>
+              <a style={menuActive ? { color: "#364753" } : color} href={href}>
+                {texto}
+              </a>
             </li>
           ))}
         </ul>
